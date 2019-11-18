e8edcc1c05429e98ff537309bdd603182b32a2b4
TergBug <tergbug@gmail.com>
2019-11-18 17:26:05 +0200

    Initial commit

--- src/test/java/org/mycode/PlayerTest.java
+++ src/test/java/org/mycode/PlayerTest.java
@@ -1,8 +1,21 @@
 package org.mycode;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PlayerTest {
-
+    private Player testedPlayer = new Player("");
+    private String testedName = "Fred";
+    private RPSElement testedRPSElement = RPSElement.ROCK;
+    @Test
+    public void shouldSetupFields(){
+        testedPlayer.setName(testedName);
+        assertEquals(testedName, testedPlayer.getName());
+        testedPlayer.setChosenElement(testedRPSElement);
+        assertEquals(testedRPSElement, testedPlayer.getChosenElement());
+        testedPlayer.setWinner(true);
+        assertTrue(testedPlayer.isWinner());
+    }
 
 }
