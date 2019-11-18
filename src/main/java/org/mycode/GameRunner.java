5c706e42d5320e1e2dc5e73e41c6a38315ee12c7
TergBug <tergbug@gmail.com>
2019-11-18 17:30:39 +0200

    Initial commit

--- src/main/java/org/mycode/GameRunner.java
+++ src/main/java/org/mycode/GameRunner.java
@@ -1,13 +1,8 @@
 package org.mycode;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
-
 public class GameRunner {
     public static void main(String[] args) {
         GameLogic game = new GameLogic();
         game.playGame(System.in, System.out);
-        //game.playLogicOnce(new ByteArrayInputStream("Fhjh\np".getBytes()), System.out);
-        //game.playGame(new ByteArrayInputStream("Fred\np\nn".getBytes()), System.out);
     }
 }
